f28218b39eb71ca4faedf6eb09714f6df04014e2
Benjamin Chambers <benjamin.chambers1@ingka.com>
2021-06-07 20:57:38 +0100

    update connection

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-app.use(cors({credentials: true, origin: 'http://18.216.160.160/'}));
 
+let test = true;
+app.use(cors({credentials: true, origin: test ? 'http://localhost:8080' : 'http://18.216.160.160/'}));
 console.log('Server Started');
-
-//example route
 
 app.get('/', (req, res) => {
     res.send('Welcome to the be template api')
