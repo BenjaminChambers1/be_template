63a401a4ad24b12080f332dd244e693285ba3010
Benjamin Chambers <benjamin.chambers1@ingka.com>
2021-06-16 22:18:32 +0100

    .

--- knexfile.js
+++ knexfile.js
@@ -23,11 +23,11 @@ module.exports = {
   development: {
     client: 'pg',
     connection: {
-      host: '18.216.160.160',
-      port: 5432,
-      database: 'db_template',
-      user:     'ben',
-      password: 'Starlight1202'
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      database: process.env.DB_DATABASE,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD
     },
     pool: {
       min: 2,
